ad8413f7be8e76f2da2de79c417a8eeec499722e
mohamed bouaroukane <mzd309712@gmail.com>
2025-10-25 08:49:18 +0100

    Make the API url static

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 
 
 const axiosClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: "https://wea-api-production.up.railway.app/api/v1/",
     timeout: 5000,
 });
 
