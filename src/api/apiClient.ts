9731df810cfa8facecdc4a9068dbf7aa169291c8
mohamed bouaroukane <mzd309712@gmail.com>
2026-01-23 23:30:55 +0100

    fix api url

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 
 
 const axiosClient = axios.create({
-    baseURL: "https://wea-api-production.up.railway.app/api/v1/",
+    baseURL: "http://72.62.232.43:6060/api/v1/",
     timeout: 5000,
 });
 
